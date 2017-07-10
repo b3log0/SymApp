42b2f0b8d4dad848a9782e8b3070794a99fd97d9
Van <lly219@gmail.com>
2017-07-10 20:49:23 +0800

    :construction: use props-type

--- app/components/list/item/index.js
+++ app/components/list/item/index.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 import ArticleItem from './Article';
 import CommentItem from './Comment';
