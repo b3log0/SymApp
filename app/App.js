97ebd7b499a80633363df9d7370c0a6f44ad2932
lxl <lxlzero@163.com>
2017-07-13 10:02:59 +0800

    添加极光消息

--- app/App.js
+++ app/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'mobx-react/native';
+import JPushModule from 'jpush-react-native';
 
 import Root from './router';
 import store from './stores';
 
-const SymApp = () => (
-  <Provider {...store}>
-    <Root />
-  </Provider>
-);
+
+class SymApp extends Component {
+  componentDidMount() {
+    // JPushModule.notifyJSDidLoad();
+    // JPushModule.addReceiveCustomMsgListener((message) => {
+    //   this.setState({ pushMsg: message });
+    // });
+    JPushModule.addReceiveNotificationListener((message) => {
+      console.log(`receive notification: ${message}`);
+    });
+  }
+
+  componentWillUnmount() {
+    // JPushModule.removeReceiveCustomMsgListener();
+    JPushModule.removeReceiveNotificationListener();
+  }
+  render() {
+    return (
+      <Provider {...store}>
+        <Root />
+      </Provider>
+    );
+  }
+}
 
 export default SymApp;
