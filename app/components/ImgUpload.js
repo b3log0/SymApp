7fc1dcc3c4165e945300f0a66d7ed7c275b32c9f
lxl <lxlzero@163.com>
2017-07-23 16:11:46 +0800

    七牛图片上传

--- app/components/ImgUpload.js
+++ app/components/ImgUpload.js
@@ -7,77 +7,77 @@ Conf.SECRET_KEY = '7YO_Iz0vZklV0Z7xIBETyaF199_DI8qNo4wFu3sd';
 const BUKET = 'symphony-dev';
 
 const options = {
-    title: '选择图片',
-    cancelButtonTitle: '取消',
-    takePhotoButtonTitle: '拍照',
-    chooseFromLibraryButtonTitle: '图片库',
-    cameraType: 'back',
-    mediaType: 'photo',
-    videoQuality: 'high',
-    durationLimit: 10,
-    maxWidth: 600,
-    maxHeight: 600,
-    aspectX: 2,
-    aspectY: 1,
-    quality: 0.8,
-    angle: 0,
-    allowsEditing: false,
-    noData: false,
-    storageOptions: {
-        skipBackup: true,
-        path: 'images'
-    }
+  title: '选择图片',
+  cancelButtonTitle: '取消',
+  takePhotoButtonTitle: '拍照',
+  chooseFromLibraryButtonTitle: '图片库',
+  cameraType: 'back',
+  mediaType: 'photo',
+  videoQuality: 'high',
+  durationLimit: 10,
+  maxWidth: 600,
+  maxHeight: 600,
+  aspectX: 2,
+  aspectY: 1,
+  quality: 0.8,
+  angle: 0,
+  allowsEditing: false,
+  noData: false,
+  storageOptions: {
+    skipBackup: true,
+    path: 'images'
+  }
 };
 
 const ImgUpload = {
-    IMG_HOST: 'http://7xjz0r.com1.z0.glb.clouddn.com',
-    imgPick() {
-        return new Promise((resolve, reject) => {
-            ImagePicker.showImagePicker(options, (response) => {
-                console.log('Response = ', response);
-                if (response.didCancel) {
-                    console.log('User cancelled image picker');
-                    reject();
-                } else if (response.error) {
-                    console.log('ImagePicker Error: ', response.error);
-                    reject();
+  IMG_HOST: 'http://7xjz0r.com1.z0.glb.clouddn.com',
+  imgPick() {
+    return new Promise((resolve, reject) => {
+      ImagePicker.showImagePicker(options, (response) => {
+        console.log('Response = ', response);
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+          reject();
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+          reject();
                     // } else if (response.customButton) {
                     //     console.log('User tapped custom button: ', response.customButton);
-                } else {
-                    resolve(response);
-                }
-            });
-        });
-    },
-    upload() {
-        const me = this;
-        return new Promise((resolve, reject) => {
-            me.imgPick().then((response) => {
-                let source;
-                if (Platform.OS === 'android') {
-                    source = { uri: response.uri, isStatic: true };
-                } else {
-                    source = { uri: response.uri.replace('file://', ''), isStatic: true };
-                }
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  },
+  upload() {
+    const me = this;
+    return new Promise((resolve, reject) => {
+      me.imgPick().then((response) => {
+        let source;
+        if (Platform.OS === 'android') {
+          source = { uri: response.uri, isStatic: true };
+        } else {
+          source = { uri: response.uri.replace('file://', ''), isStatic: true };
+        }
 
-                const putPolicy = new Auth.PutPolicy2(
+        const putPolicy = new Auth.PutPolicy2(
                     { scope: `${BUKET}:${response.fileName}` }
                 );
-                const uptoken = putPolicy.token();
-                const formInput = {
-                    key: response.fileName
+        const uptoken = putPolicy.token();
+        const formInput = {
+          key: response.fileName
                     // formInput对象如何配置请参考七牛官方文档“直传文件”一节
-                };
-                Rpc.uploadFile(source.uri, uptoken, formInput).then((res) => {
-                    console.log('upload success');
-                    resolve(res);
-                }).catch((e) => {
-                    console.log(e);
-                    reject(e);
-                });
-            });
+        };
+        Rpc.uploadFile(source.uri, uptoken, formInput).then((res) => {
+          console.log('upload success');
+          resolve(res);
+        }).catch((e) => {
+          console.log(e);
+          reject(e);
         });
-    }
+      });
+    });
+  }
 };
 
 
